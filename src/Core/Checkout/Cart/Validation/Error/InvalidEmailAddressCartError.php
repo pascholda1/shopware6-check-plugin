4d6d0ca1c01f5df63bc9312d177ee11d7cd9a63e
pascholda1 <dev@pascholda.one>
2023-06-01 14:05:44 +0200

    * fixed InvalidEmailAddressCartError

--- src/Core/Checkout/Cart/Validation/Error/InvalidEmailAddressCartError.php
+++ src/Core/Checkout/Cart/Validation/Error/InvalidEmailAddressCartError.php
@@ -33,4 +33,15 @@ class InvalidEmailAddressCartError extends AbstractInvalidAddressCartError
     ];
     
     
+    public function __construct(string $resultText)
+    {
+        $this->resultTextPartialKey = self::CHECK_RESULT_PARTIAL_KEY_MAP[trim($resultText)] ?? $resultText;
+        
+        parent::__construct();
+    }
+    
+    public function getMessageKey(): string
+    {
+        return implode('-', [self::KEY, $this->resultTextPartialKey]);
+    }
 }
