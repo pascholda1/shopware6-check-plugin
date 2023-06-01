88bc8a7c29054c65fd6866c847c17f0d599b2980
pascholda1 <dev@pascholda.one>
2023-06-01 14:21:13 +0200

    * fixed InvalidNameCartError

--- src/Core/Checkout/Cart/Validation/Error/InvalidNameCartError.php
+++ src/Core/Checkout/Cart/Validation/Error/InvalidNameCartError.php
@@ -7,10 +7,18 @@ class InvalidNameCartError extends AbstractInvalidAddressCartError
     protected const KEY = 'adresslabor-invalid-name-blocked';
     
     protected const CHECK_RESULT_PARTIAL_KEY_MAP = [
-        'ungültig, PLZ und Ort OK' => 'invalid-street',
-        'Ungültig, mehrdeutig' => 'ambiguous',
-        'Ungültig' => 'invalid',
-        'Bitte Eingabe prüfen' => 'dubious',
+        'Dubiose Eingabe' => 'dubious',
     ];
     
+    public function __construct(string $resultText)
+    {
+        $this->resultTextPartialKey = self::CHECK_RESULT_PARTIAL_KEY_MAP[trim($resultText)] ?? $resultText;
+        
+        parent::__construct();
+    }
+    
+    public function getMessageKey(): string
+    {
+        return implode('-', [self::KEY, $this->resultTextPartialKey]);
+    }
 }
